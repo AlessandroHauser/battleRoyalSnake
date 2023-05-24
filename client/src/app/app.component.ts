9597e634137d3eb405993daebab8df751c15e735
fynnhaenybbzbl <108476679+fynnhaenybbzbl@users.noreply.github.com>
2023-05-24 16:00:02 +0200

    add first grid + routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'SNAKE BATTLEROYALE!';
+export class AppComponent implements OnInit{
+  title = 'Snake Battleroyale!';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    var canvas = <HTMLCanvasElement> document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    var squareSize = 10;
+
+    var squaresHorizontal = 100;
+    var squaresVertical = 70;
+
+    for (var x = 0; x < squaresHorizontal; x++) {
+      for (var y = 0; y < squaresVertical; y++) {
+        var squareX = x * squareSize;
+        var squareY = y * squareSize;
+
+        var isLightGreen = (x + y) % 2 === 0;
+
+        ctx!.fillStyle = isLightGreen ? "#a2d149" : "#aad751";
+        ctx!.fillRect(squareX, squareY, squareSize, squareSize);
+      }
+    }
+
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FieldComponent } from './components/field/field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FieldComponent
   ],
   imports: [
     BrowserModule,
