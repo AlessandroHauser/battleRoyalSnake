f79ee629c755871134c35a9c2eed000e080aa7be
fynnhaenybbzbl <108476679+fynnhaenybbzbl@users.noreply.github.com>
2023-05-25 09:06:23 +0200

    Changes by Shadow + Unversioned Files

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {FieldComponent} from "./components/field/field.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '**', component: AppComponent},
+  { path: 'home', component: AppComponent },
+  { path: 'field', component: FieldComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Canvas} from "canvas";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
