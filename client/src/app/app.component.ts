100d853194976dd90cb5bc41168e78634d7bef30
Alessandro Hauser <alessandro.hauser@bbzbl-it.ch>
2023-05-26 17:52:18 +0200

    connection setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {OnInit} from '@angular/core';
+import {ClientService} from './services/client.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SNAKE BATTLEROYALE!';
+
+  constructor(private clientService: ClientService) {}
+
+  ngOnInit() {
+    this.clientService.connect("ws://localhost:42069", this.receiveMessage);
+  }
+
+  receiveMessage(event: MessageEvent): void {
+      const message: string = event.data;
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  sendInput(event: KeyboardEvent) {
+    this.clientService.userInput(event.key);
+  }
 }
