ffae23840c0df39d4d997336a2a8f9eda95132ae
fynnhaenybbzbl <108476679+fynnhaenybbzbl@users.noreply.github.com>
2023-05-31 13:25:41 +0200

    removed unused things

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {Canvas} from "canvas";
-import {Router} from "@angular/router";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
 
-  constructor(private router: Router) {
-  }
-
-  ngOnInit() {
-
+  constructor() {
   }
 }
